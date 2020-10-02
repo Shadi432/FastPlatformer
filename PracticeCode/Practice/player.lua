cee794977ba719537e6ccf999f38f78b0a4cd627
Shadi432 <30848732+Shadi432@users.noreply.github.com>
2020-10-02 10:10:52 +0100

    Fixed double jump after walking off platform
    Fixed the player being able to double jump after walking off a platform, even if they're still in the air.

---
+++ PracticeCode/Practice/player.lua
@@ -0,0 +1,37 @@
+Player = Entity:extend()
+
+function Player:new(x,y)
+   Player.super.new(self,x,y, "player.png") 
+   
+   self.strength = 50
+   self.canJump = false
+end
+
+function Player:update(dt)
+    Player.super.update(self,dt)
+    
+    if love.keyboard.isDown("left") then
+        self.x = self.x - 200 * dt
+    elseif love.keyboard.isDown("right") then
+        self.x = self.x + 200 * dt
+    end
+    
+    if self.last.y ~= self.y then -- If their y has changed from the last recorded one, they're not on the ground
+        self.canJump = true
+    end
+end
+
+function Player:jump(dt)
+    if self.canJump then
+       self.gravity = -600
+       self.canJump = false
+    end
+end
+
+function Player:collide(e,direction)
+   Player.super.collide(self, e, direction)
+   
+    if direction == "bottom" then
+        self.canJump = true
+    end
+end
